82a7f5836a68eff5993d5423539e1792eb34cb12
Timo Vuorijarvi <tim.vuorijarvi@gmail.com>
2024-03-10 14:21:38 +0200

    Closes #1. Fixed mysql delimiter to be handled in file scope. Added delimiter unit test

---
+++ src/test/data/delimiter-b.sql
@@ -0,0 +1,3 @@
+DELIMITER #
+create table DELIM_B ( ID integer )#
+create table DELIM_B2 ( ID integer )#
---
+++ src/test/data/delimiter-c.sql
@@ -0,0 +1,3 @@
+DELIMITER £
+create table DELIM_C ( ID integer )£
+create table DELIM_C2 ( ID integer )£
